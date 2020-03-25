5bffacdc5f73f98e29d5c29967fe15e6be8c6745
Kaleem Ullah <kaleemullah786.ku61@gmail.com>
2020-03-25 20:24:08 +0500

    Routesd Plan

--- src/components/RoutesPlan/RoutesPlan.js
+++ src/components/RoutesPlan/RoutesPlan.js
@@ -1,17 +1,24 @@
-import React,{ Component } from "react";
-
-
-class RoutesPlan extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            routes:null
-        }
-    }
-    render(){
-        return (
-            <h1>Route Plannong</h1>
-        )
-    }
+import React, {Component} from 'react';
+import {Form} from 'react-bootstrap';
+class RoutesPlan extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      routes: null,
+    };
+  }
+  render() {
+    return (
+      <div className="row mt-3 no-gutters">
+        <div className="col-md-6">
+          <div className="row no-gutters">
+            <div className="col-md-4 mr-3n">
+              <b>Routes > Trip Planning ></b>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-export default RoutesPlan
+export default RoutesPlan;
